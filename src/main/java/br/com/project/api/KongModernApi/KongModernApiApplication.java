b150efa534c375d576209699394e692d2ef5d724
tulio.en91@gmail.com <tulio.en91@gmail.com>
2024-04-19 10:40:27 -0300

    (SPRING STARTING RUNNING PROJ API)
    Primeiro commit estrutura API

---
+++ src/main/java/br/com/project/api/KongModernApi/KongModernApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.api.KongModernApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongModernApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongModernApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/project/api/KongModernApi/KongModernApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.api.KongModernApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongModernApiApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
